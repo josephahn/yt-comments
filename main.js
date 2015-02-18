e06efc3d70853f961eb7c03aa6260b99169bd155
Joseph Ahn <joseph.t.ahn@gmail.com>
2015-02-17 19:52:40 -0500

    show error in popup if URL is not valid

--- main.js
+++ main.js
@@ -20,12 +20,21 @@ function getVideoId(url) {
 
   var re = /https?:\/\/www.youtube.com\/watch\?v=([^&]+)/;
   var found = url.match(re);
-  return found ? found[1] : 'URL not valid. Could not retrieve video ID.';
+  return found ? found[1] : null;
 
+}
+
+function renderStatus(statusText) {
+  document.getElementById('status').textContent = statusText;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   getCurrentTabUrl(function(url) {
-    alert(getVideoId(url));
+    var videoId = getVideoId(url);
+    if (videoId) {
+      // make GET request
+    } else {
+      renderStatus('URL not valid. Could not retrieve video ID.');
+    }
   });
 });
